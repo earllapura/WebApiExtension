88c5feafa60865333b9eb1d8b47473ae3f164eaa
earllapura <earlvon.lapura@g.msuiit.edu.ph>
2018-01-15 14:44:42 +0800

    Made some variables in context extensible to inheriting classes

--- src/Context/WebApiContext.php
+++ src/Context/WebApiContext.php
@@ -30,29 +30,29 @@ class WebApiContext implements ApiClientAwareContext
     /**
      * @var string
      */
-    private $authorization;
+    protected $authorization;
 
     /**
      * @var ClientInterface
      */
-    private $client;
+    protected $client;
 
     /**
      * @var array
      */
-    private $headers = array();
+    protected $headers = array();
 
     /**
      * @var \GuzzleHttp\Message\RequestInterface|RequestInterface
      */
-    private $request;
+    protected $request;
 
     /**
      * @var \GuzzleHttp\Message\ResponseInterface|ResponseInterface
      */
-    private $response;
+    protected $response;
 
-    private $placeHolders = array();
+    protected $placeHolders = array();
 
     /**
      * {@inheritdoc}
@@ -332,7 +332,7 @@ class WebApiContext implements ApiClientAwareContext
      *
      * @return string
      */
-    private function prepareUrl($url)
+    protected function prepareUrl($url)
     {
         return ltrim($this->replacePlaceHolder($url), '/');
     }
@@ -408,7 +408,7 @@ class WebApiContext implements ApiClientAwareContext
         }
     }
 
-    private function sendRequest()
+    protected function sendRequest()
     {
         try {
             $this->response = $this->getClient()->send($this->request);
@@ -421,7 +421,7 @@ class WebApiContext implements ApiClientAwareContext
         }
     }
 
-    private function getClient()
+    protected function getClient()
     {
         if (null === $this->client) {
             throw new \RuntimeException('Client has not been set in WebApiContext');
